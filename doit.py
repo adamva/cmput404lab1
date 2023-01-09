1e75e77b6dd0f56c1440b7274a96723645047d32
Adam Ahmed <avahmed@cs.ualberta.ca>
2023-01-09 15:16:03 -0700

    download and print github source code of ownself

--- doit.py
+++ doit.py
@@ -3,3 +3,6 @@ req_ver = requests.__version__
 print('requests version: ' + req_ver)
 g_home = requests.get('https://www.google.com')
 # print(g_home)
+
+src_code = requests.get('https://raw.githubusercontent.com/adamva/cmput404lab1/master/doit.py')
+print('\n' + src_code.text)
